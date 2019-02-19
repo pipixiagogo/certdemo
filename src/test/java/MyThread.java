2ddc00a8bd66ab70db8d164a2728d8426ad4f0ee
zh <username@example.com>
2019-02-19 09:45:37 +0800

    线程池与同步锁的示例代码 加16进制字节与字符串的转换

--- src/test/java/MyThread.java
+++ src/test/java/MyThread.java
@@ -14,6 +14,24 @@ public class MyThread {
             System.out.println("线程池中线程数目："+threadPoolExecutor.getPoolSize()+"，队列中等待执行的任务数目："+
                     threadPoolExecutor.getQueue().size()+"，已执行完别的任务数目："+threadPoolExecutor.getCompletedTaskCount());
         }
-
+        byte b = (byte) (0x80|0x04);
+        String s = toHexString(b);
+        System.out.println(s);
+        byte b1 = parseHexString(s);
+        System.out.println(b1);
+    }
+    //单个字节转为16进制的字符串
+    public static String toHexString(byte b) {
+        String s = Integer.toHexString(b & 0xFF);
+        int len = s.length();
+        if (len < 2) {
+            s = "0" + s;
         }
+        return s.toUpperCase();
+    }
+    //字符串转为16进制的单个字节
+    public static byte parseHexString(String s) {
+        int i = Integer.parseInt(s, 16);
+        return (byte) i;
+    }
 }
